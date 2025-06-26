df3135600bd832db6e228ccb3bbb0f5105fd7ee6
David Reisinho <davidreisinho45@gmail.com>
2025-06-26 19:18:22 +0100

    Novo update ao BusinessContext e ao IBusinessContext

--- Triopet/Triopet.BusinessContext/BusinessContext.cs
+++ Triopet/Triopet.BusinessContext/BusinessContext.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Triopet.BusinessContext.Entities;
 
 namespace Triopet.BusinessContext
 {
@@ -16,5 +17,16 @@ namespace Triopet.BusinessContext
 
             optionsBuilder.UseSqlServer("Data Source=SQL1004.site4now.net;Initial Catalog=db_abaa30_dotnet003;User Id=db_abaa30_dotnet003_admin;Password=YourstrongP@ssword1");
         }
+        public DbSet<AnimalType> AnimalTypes { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Entry> Entries { get; set; }
+        public DbSet<Exit> Exits { get; set; }
+        public DbSet<Motif> Motifs { get; set; }
+        public DbSet<Image> Images { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductEntry> ProductEntries { get; set; }
+        public DbSet<ProductExit> ProductExits { get; set; }
+
     }
+
 }
