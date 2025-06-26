d64121da7b9676ec13c4c22ecb92730efe63c1af
David Reisinho <davidreisinho45@gmail.com>
2025-06-26 17:42:47 +0100

    Classes feitas

---
+++ Triopet/Triopet.BusinessContext/Entities/Motif.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triopet.BusinessContext.Entities
+{
+    public class Motif
+    {
+        public int MotifId { get; set; }
+        public string Reason { get; set; }
+    }
+}
