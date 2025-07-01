bb024da87b4be2ae2c3b48eeb513f6d1897b52cf
Franciele Silva <fdecassiafurtadosilva@gmail.com>
2025-07-01 18:44:49 +0100

    temp

---
+++ Triopet/Triopet.Web/IApiService.cs
@@ -0,0 +1,23 @@
+﻿using Refit;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Triopet.Web.Models;
+
+namespace Triopet.Web
+{
+    public interface IApiService
+    {
+        [Get("/products")]
+        Task<List<Product>> GetProductsAsync();
+
+        [Post("/products")]
+        Task<HttpResponseMessage> AddProductAsync([Body] Product product);
+
+        [Put("/products")]
+        Task<HttpResponseMessage> UpdateProduct([Body] Product product);
+
+        [Delete("/products/{id}")]
+        Task<HttpResponseMessage> DeleteProduct(int id);
+    }
+}
